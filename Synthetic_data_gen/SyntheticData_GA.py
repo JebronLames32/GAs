d0cfaeee97a39f360e7c312be7e24a349a2f96c1
Abhinav R Cillanki <95158195+JebronLames32@users.noreply.github.com>
2022-08-16 23:29:59 +0530

    Imported libraries, need to fetch data using pandas

---
+++ Synthetic_data_gen/SyntheticData_GA.py
@@ -0,0 +1,12 @@
+import numpy as np
+import random
+import math
+import pandas
+
+#elements will contain value of bitcoin trade data
+Number_of_elements_in_gen=500
+#500 random values shall be sampled over a large dataset obtained over an interval of one month
+
+# Add code to generate list from excel file using pandas here
+
+##
